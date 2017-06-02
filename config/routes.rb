983a07ca097284ff94fbcaea9c18c0413cc79b98
uosGH <jkl9510357@gmail.com>
2017-06-02 14:11:00 +0000

    image

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   
+  devise_for :users
   root 'main#index'
   get  'main/login'
+  get '/create_post' => 'main#create_post'
+  get '/create_comment/:post_id' => 'main#create_comment'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
